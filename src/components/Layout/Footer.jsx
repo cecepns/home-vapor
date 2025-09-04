813a3d45b4d9bb62f92c053af87318aa6e92eab3
Cecep Nandang <cecep@kima.one>
2025-09-04 19:38:52 +0700

    fix: alamat

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-start space-x-2">
                 <MapPin size={16} className="mt-1 text-blue-400" />
-                <span className="text-gray-300 text-sm">
+                <span className="text-gray-300 text-sm block max-w-44">
                   Jl. Ahmad Yani, simpang 4 RS Amalia, Kelurahan Api-Api, Bontang Baru
                 </span>
               </div>
